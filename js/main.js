b939791b4d0bc909d2fdfd6925a8fbe4c28dc125
Cássia Costa <cassia.silva93@hotmail.com>
2022-04-07 17:07:06 -0300

    Updates

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import { calculos, rarityFilter, eggsFilter, typeFilter, alphabeticalFilter, sea
 import data from '../data/pokemon/pokemon.js';
 
 let allPokemons = data.pokemon;
+const shinyUrlImg = "https://www.serebii.net/pokemongo/pokemon/shiny/";
 
 const selectType = document.getElementById('typeFilter');
 const selectRarity = document.getElementById('rarityFilter');
@@ -66,45 +67,52 @@ function translate(type) {
 
 function showPokemon(data) {
   const allCards = data.map((item) => `
-      <div class="cards" id="cards">
-          <section class="front-cards" id="frontCards">
-              <p class="numberPokemon">${item.num}</p>
-              <picture>
-                  <img class="image-card" src="${item.img}" alt="imagem do Pokémon" loading = "lazy">
-              </picture>
-              <div class="info-cards">
-                  <p class="namePokemon">${item.name}</p>
-                  <p class="typePokemon"> <b>Tipo:</b> ${item.type.map(element => {
-                    return translate(element)
-                  })}</p>
-                  <p class="regionPokemon"> <b>Região:</b> ${item.generation["name"]}</p>
-              </div>
-          </section>
-      </div>
+    <div class="cards" id="cards">
+        <section class="front-cards" id="frontCards">
+          <p class="numberPokemon">${item.num}</p>
+          <picture>
+              <img class="image-card" src="${item.img}" alt="imagem do Pokémon" loading = "lazy">
+          </picture>
+          <div class="info-cards">
+              <p class="namePokemon">${item.name}</p>
+              <p class="typePokemon"> <b>Tipo:</b> ${item.type.map(element => {
+                return translate(element)
+              })}</p>
+              <p class="typePokemon"> <b>Resistências:</b> ${item.resistant.map(element => {
+                return translate(element)
+              })}</p>
+              <p class="typePokemon"><b>Ovos:</b>${item.egg}</p>
+          </div>
+        </section>
+    </div>
     `).join('')
     document.getElementById('pokemonList').innerHTML = allCards;
 }
 
-// function showPokemonShiny(data) {
-//   const allCards = data.map((item) => `
-//       <div class="cardsShiny">
-//           <section class="front-cardsShiny" id="frontCardsShiny">
-//               <p class="numberPokemonShiny">${item.num}</p>
-//               <picture>
-//                   <img class="image-cardShiny" src="${item.img}" alt="imagem do Pokémon" loading = "lazy">
-//               </picture>
-//               <div class="info-cardsShiny">
-//                   <p class="namePokemonShiny">${item.name}</p>
-//                   <p class="typePokemonShiny"> <b>Tipo:</b> ${item.type.map(element => {
-//                     return translate(element)
-//                   })}</p>
-//                   <p class="regionPokemonShiny"> <b>Região:</b> ${item.generation["name"]}</p>
-//               </div>
-//           </section>
-//       </div>
-//     `).join('')
-//     document.getElementById('pokemonList').innerHTML = allCards;
-// }
+function showPokemonShiny(data) {
+  const allCards = data.map((item) => `
+      <div class="cards">
+        <section class="front-cards-shiny" id="frontCards">
+          <p class="numberPokemon">${item.num}</p>
+          <picture>
+            <img class="image-card" src="${shinyUrlImg}${item.img.slice(42,50)}" alt="imagem do Pokémon" loading = "lazy">
+          </picture>
+          <div class="info-cards">
+            <p class="namePokemon">${item.name}</p>
+            <p class="typePokemon"> <b>Tipo:</b> ${item.type.map(element => {
+              return translate(element)
+              })}
+            </p>
+            <p class="typePokemon"> <b>Resistências:</b> ${item.resistant.map(element => {
+              return translate(element)
+            })}</p>
+            <p class="typePokemon"><b>Ovos:</b>${item.egg}</p>
+          </div>
+        </section>
+      </div>
+  `).join('')
+    document.getElementById('pokemonList').innerHTML = allCards;
+}
 
 function searchByType(e) {
   allPokemons = typeFilter(allPokemons, e.target.value)
@@ -114,10 +122,14 @@ function searchByType(e) {
 }
 
 function searchByRarity(e) {
-  allPokemons = rarityFilter(allPokemons, e.target.value)
-  calculationBar.innerHTML = `Esta raridade de pokémon representa ${calculos(data.pokemon.length, allPokemons.length)}% 
-    do total`
-  showPokemon(allPokemons)
+  if (e.target.value == "shiny") {
+    return showPokemonShiny(allPokemons)
+  } else {
+    const resultRarity = rarityFilter(allPokemons, e.target.value)
+    calculationBar.innerHTML = `Esta raridade de pokémon representa ${calculos(allPokemons.length, resultRarity.length)}% 
+        do total`
+  showPokemon(resultRarity)
+  }
 }
 
 function searchByEgg(e) {
